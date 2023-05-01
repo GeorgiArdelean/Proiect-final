53a4c6941fa46b1f4bff7f5dfb27ce7bedbbccee
GeorgiArdelean <121879612+GeorgiArdelean@users.noreply.github.com>
2023-05-01 13:50:37 +0300

    Add files via upload

---
+++ src/routes/index.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import { AppLayout } from "../layouts/App.Layout";
+import AuthLayout from "../layouts/AuthLayout";
+
+import * as Pages from "../pages";
+
+export default function () {
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Pages.Home />} />
+        <Route path="/manage" element={<Pages.ManageBooks />} />
+        <Route path="/book/:id" element={<Pages.Book />} />
+      </Route>
+
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Pages.Login />} />
+        <Route path="/register" element={<Pages.Register />} />
+      </Route>
+    </Routes>
+  );
+}
